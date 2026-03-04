dc0c6d34482879d03cebc720b88d92004e8ba471
Tonksthebear <jconigliari@ironarchtech.com>
2026-03-03 16:27:24 -0800

    chore: bump version to v0.1.21-rc.1

--- cli/main.go
+++ cli/main.go
@@ -22,7 +22,7 @@ import (
 	"github.com/peregrine-digital/activate-framework/cli/tui"
 )
 
-const version = "0.1.20-rc.1"
+const version = "0.1.21-rc.1"
 
 type cliArgs struct {
 	command      string
